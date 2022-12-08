bc4e9bc336caeceb31f005bf084837100653a7e5
bagasysf <bagasysf@gmail.com>
2022-12-08 23:58:02 +0700

    bagasysf: rapihin shadow, cart, dan checkout

--- checkout.js
+++ checkout.js
@@ -6,9 +6,9 @@ const totalPriceQty = document.getElementById("totalPriceQty")
 
 let totalPrice = 0
 let qty = 0
-for(let i = 0; i < dataBase.length; i++){
-    if(dataBase[i].cart){
-        purchasedGame.innerHTML+= `<li class="flex gap-[20px] py-[20px] pl-[20px] pr-[20px] cursor-pointer">
+for (let i = 0; i < dataBase.length; i++) {
+    if (dataBase[i].cart) {
+        purchasedGame.innerHTML += `<li class="flex gap-[20px] py-[20px] pl-[20px] pr-[20px] cursor-pointer">
         <div class="w-[70px] h-[70px]">
             <img class="h-full" src="/assets/gameCover/${dataBase[i].img}" alt="gambar-thewitcher-cart">
         </div>
@@ -17,9 +17,9 @@ for(let i = 0; i < dataBase.length; i++){
             <p class="text-lg">Rp ${dataBase[i].price}</p>
         </div>
     </li>`
-    totalPrice += dataBase[i].price
-    qty++
-    totalPriceQty.innerHTML = `<li
+        totalPrice += dataBase[i].price
+        qty++
+        totalPriceQty.innerHTML = `<li
     class="flex justify-between py-[10px] pl-[20px] pr-[20px] hover:text-[#4F76FF] cursor-pointer text-white font-bold text-lg">
     <p class="text-[#4F76FF] text-lg">Quantity: </p>
     <p class="text-lg">${qty}</p>
